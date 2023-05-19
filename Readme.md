d832d7610dbb5b126b8fd9349f7cca314183a0bd
Vinícius Araújo <varaujopacheco@gmail.com>
2023-05-19 20:07:57 -0300

    Tratamento de erros adicionado e cores no terminal

--- Readme.md
+++ Readme.md
@@ -2,3 +2,5 @@
 - Escolha um número de 0 a 9
 - Pode escrever o nome de cada número (em pt-BR)
 - Tem uma quantidade aleatória de tentativas
+- Adicionado o tratamento de erros que poderiam quebrar o jogo
+- Apresentando cores aleatórias no terminal
--- while.py
+++ while.py
@@ -1,46 +1,56 @@
 # Jogo da Adivinhação que mede sua sorte ao encontrar o número secreto
 from random import randint
 
-sorte = randint(1,100)          # A dificuldade também é aleatória HAHAHA
-limite = 0
-tupla = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez')
-secreto = randint(0, 10)
-escolhido = None
-print("""
------------------------------------------------------------------------------------------
-|                  BEM-VINDO(A) AO JOGO DE ADIVINHAÇÃO PUNITIVO                         |
------------------------------------------------------------------------------------------
-""")
+tenta_denovo = True
+tupla = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove')
 
+def inicia_jogo(integer):        
+    sorte = randint(1,100)  # A dificuldade também é aleatória HAHAHA (sorte = 0:  GAME OVER)
+    limite = 0
+    secreto = randint(0, 9)
+    escolhido = None        # Não pode ser número porque vai gastar chance do usuário ser pegar input
+    print(f"""\033[3{randint(1, 7)}m
+    -----------------------------------------------------------------------------------------
+    |                  BEM-VINDO(A) AO JOGO DE ADIVINHAÇÃO PUNITIVO                         |
+    -----------------------------------------------------------------------------------------
+    """)                    # A cada vez que o jogo é executado, troca a cor dos caracteres
+    while secreto != escolhido and sorte > limite:
+        try:
+            escolhido = input('Escolha um número de 0 a 9: ').lower()
+            escolhido = integer(escolhido)      
 
-while secreto != escolhido and sorte > limite:
-    try:
-        escolhido = input('Escolha um número de 0 a 9: ').lower()
-        if escolhido in tupla:
-            escolhido = tupla.index(escolhido)
-
-        escolhido = int(escolhido)
-
-    except:
-        sorte //= 2 # Lembram da parte punitiva?
-        print(f'Sem um número, não tem graça!\nPode usar o cardinal como por extenso')
+        except ValueError: 
+            print('\tSem um número, não tem graça!\nPode usar o cardinal como por extenso\n')
         
-    
-    if escolhido != secreto:
-        sorte //= 2
-        #print(f'Sua sorte caiu para {sorte}%')  # A idéia inicial era o jogador não saber quantas tentativas ele ainda tem
+        else:
+            if escolhido not in range(0, 10):
+                print('\tO número precisa estar entre 0 e 9!\n')
+            
+        finally:
+            if escolhido != secreto:
+                sorte //= 2
+
+    if secreto == escolhido:
+        print(f'\n\t\tParabéns! Você descobriu o {tupla[secreto]} com {sorte}% de sorte\n')
+                
+    else:
+        print('\t\t\tGAME OVER:\n\t\t\tNão descobriu o número secreto !!!\n')
 
 
-if secreto == escolhido:
-    print(f"""
------------------------------------------------------------------------------------------
-|                         PÓDIO           DO          VENCEDOR                          |
------------------------------------------------------------------------------------------
-\n\t\tParabéns! Você descobriu o {tupla[secreto]} com {sorte}% de sorte\n""")
-              
-else:
-    print("""
------------------------------------------------------------------------------------------
-|                               GAME               OVER                                 |
------------------------------------------------------------------------------------------
-\n\t\t\tNão descobriu o número secreto !!!\n""")
+def trata_bool(option):
+    if option[0:1] == 'n':
+        return False
+    else: 
+        return True
+
+
+def trata_int(number):
+    if number in tupla:
+        number = tupla.index(number)
+
+    return int(number)
+
+
+while tenta_denovo:
+    inicia_jogo(trata_int)
+    tenta_denovo = trata_bool(input('Tentar outra vez? (s/n) ').lower())
